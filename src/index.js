a373e3310594526aebdd1e10c4a5edbac45282ec
Nazar Ashirov <newnewqwes@gmail.com>
2020-03-14 20:15:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, config) {
+
+  let option = config.map(item => item.join(''));
+  let testItem = "";
+  let sub = str;
+
+  for (let j = 0; j < str.length; j++) {
+      for (let i = 0; i < option.length; i++) {
+          if (sub.includes(option[i])) { //Метод includes() определяет, содержит ли массив определённый элемент, возвращая в зависимости от этого true или false.
+              testItem = option[i];
+
+              sub = sub.replace(testItem, "");
+              i--;
+          }
+      }
+  }
+  if (sub === '') return true;
+   else return false;
+};
